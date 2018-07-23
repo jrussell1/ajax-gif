d75028cd72aaee19a0d25c1ad5df69a098473640
Jordan Russell <jordanrussell1@gmail.com>
2018-07-23 19:25:11 -0400

    updated html with basic necessary buttons & pseudo-code for how to go about code

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Create an array of strings related to a topic. Save it to var topics
+
+//take the topics in the array and create buttons in html
+    //try using a loop append a button for each string
+
+//when user clicks a button page grabs 10 STATIC gif images from the API and places on page
+    //under every gif should display rating
+
+//when click on one of images gif should animate. when clicked again it becomes static again
+
+//add form to the pg that takes in value from user input box and adds it to topics array
+    //make a function call that takes each topic in array and remakes the buttons on the page
+
+//make mobile responsibe
+
+//allow users to request additional gifs to be added to the page - each request should add 10 gifs - not 
+    //overwrite the existing ones
